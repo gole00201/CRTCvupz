d713daba6ca93eab4926c572c707a2fee7524f4b
Volkov Egor Desctop Home <gole00201@gmail.com>
2023-07-10 22:15:34 +0300

    Добавил виджет вупза

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from Src.armVupCfgLoad.armLoadJson import ParseJson
+from Src.armVupUi.UiEntity.MainWindow import ArmMainwindow
 
 if __name__ == "__main__":
     json_path = './Cfg/SaintPSortMsk-3/stp.json'
     tp = ParseJson(json_path).create_tp_from_json()
-    for way in tp.ways:
-        print(f"Путь номер: {way.way_num} содержит {way.vupz_cnt} вупзов: ")
-        for vupz in way.vupz_obj:
-            print(vupz.name)
+    window = ArmMainwindow(tp)
+    window.create_context()
+    window.mainloop()
